500f471ea3fa630cdb32deffc1d205d0dbab0929
unknown <rajuh301@gmail.com>
2026-02-12 08:20:28 +0600

    update all setting

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-export default prisma;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
