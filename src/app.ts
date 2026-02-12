0a4f3c159388bf2a3c298b758d6c6e1946341f89
unknown <rajuh301@gmail.com>
2026-02-12 08:44:38 +0600

    Solve error

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,40 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express, { Application, Request, Response, NextFunction } from "express"
+import cors from "cors"
 import router from "./app/routes";
 import globalErrorHandler from "./app/middleWares/globalErrorHandlere";
+import cookieParser from 'cookie-parser'
 
-const app = express();
-
+const app: Application = express();
 app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send({ message: "Markating-agency server is running successfully!" });
+//Purser
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Markating-agency server is running successfully!"
+    })
 });
+
 
 app.use("/api/v1/", router);
+
 app.use(globalErrorHandler);
 
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "API NOT FOUND!",
-    error: {
-      path: req.originalUrl,
-      message: "Your requested path is not found!",
-    },
-  });
-});
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+
+        sussess: false,
+        message: "API NOT FOUND!",
+        error: {
+            patah: req.originalUrl,
+            message: "Your requested path is not found!"
+        }
+
+    })
+})
+
 
 export default app;
--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,11 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
+export default prisma;
