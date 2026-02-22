989f55fb0e4160d5464060506fa9b715fbc6fc96
unknown <rajuh301@gmail.com>
2026-02-22 11:16:59 +0600

    update enum

--- src/app/modules/Role/role.route.ts
+++ src/app/modules/Role/role.route.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import { RoleController } from './role.controller';
 import { RoleValidation } from './role.validation';
-import { UserRole } from '@prisma/client';
 import validateRequest from '../../middleWares/validateRequest';
 import auth from '../../middleWares/auth';
+import { ENUM_USER_ROLE } from '../enums/user';
 
 const router = express.Router();
 
 // রোল তৈরি করার রাউট
 router.post(
   '/create-role',
-  auth(UserRole.OWNER, UserRole.ADMIN),
+  auth(ENUM_USER_ROLE.OWNER, ENUM_USER_ROLE.ADMIN),
   validateRequest(RoleValidation.createRoleZodSchema), // ভ্যালিডেশন যুক্ত হলো
   RoleController.createRole
 );
@@ -18,7 +18,7 @@ router.post(
 // ইউজারকে রোল দেওয়ার রাউট
 router.post(
   '/assign-role',
-  auth(UserRole.OWNER, UserRole.ADMIN),
+  auth(ENUM_USER_ROLE.OWNER, ENUM_USER_ROLE.ADMIN),
   validateRequest(RoleValidation.assignRoleZodSchema), // ভ্যালিডেশন যুক্ত হলো
   RoleController.assignRoleToUser
 );
