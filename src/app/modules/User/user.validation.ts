6428f38b1dbb28baaf54d8149f434cab58c8e9bc
unknown <rajuh301@gmail.com>
2026-03-01 13:07:55 +0600

    solve signup

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 export const UserValidation = {
   register: z.object({
     body: z.object({
-      fullName: z.string().min(3, "Full name is required"),
+      fullName: z.string()("Full name is required"),
       email: z.string().email("Invalid email address"),
-      password: z.string().min(6, "Password must be at least 6 characters"),
-      companyName: z.string().min(2, "Company name is required"),
+      password: z.string()("Password must be required"),
+      companyName: z.string().min(1, "Company name is required"),
     }),
   }),
   
