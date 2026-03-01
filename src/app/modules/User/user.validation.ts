7b03998ff33f755441eb838786413876e11fa697
unknown <rajuh301@gmail.com>
2026-03-01 13:45:59 +0600

    xx

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 export const UserValidation = {
   register: z.object({
     body: z.object({
-      fullName: z.string().min(1, "Full name is required"),
+      fullName: z.string().min(1,"Full name is required"),
       email: z.string().email("Invalid email address"),
-      password: z.string().min(4, "Password must be at least 4 characters"),
-      companyName: z.string().min(2, "Company name is required"),
+      password: z.string().min(4,"Password must be at least 4 characters"),
+      companyName: z.string().min(2,"Company name is required"),
     }),
   }),
   
@@ -14,7 +14,7 @@ export const UserValidation = {
   login: z.object({
     body: z.object({
       email: z.string().email("Invalid email address"),
-      password: z.string().min(1, "Password is required"),
+      password: z.string().min(1,"Password is required"),
     }),
   }),
 };
