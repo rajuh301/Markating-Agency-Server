679713c666416ac52c1cfd14b21b5a633fad21ad
unknown <rajuh301@gmail.com>
2026-03-01 13:54:15 +0600

    last 3

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -5,8 +5,8 @@ export const UserValidation = {
     body: z.object({
       fullName: z.string().min(1, "Full name is required"),
       email: z.string().email("Invalid email address"),
-      password: z.string().min(6, "Password must be at least 6 characters"),
-      companyName: z.string().min(2, "Company name is required"),
+      password: z.string().min(3, "Password must be at least 3 characters"),
+      companyName: z.string().min(1, "Company name is required"),
     }),
   }),
   
